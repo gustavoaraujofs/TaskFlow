21e252a909145bd8a446731c650ae571feaeab4a
gustavoaraujofs <gustavoaraujofs2526@gmail.com>
2024-04-21 22:01:45 -0300

    Melhorando a organização do projeto.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,6 @@
-const express = require("express");
-
-const app = express();
+const app = require("./app");
 
 const port = 3000;
-
-app.get("/", (req, res) => {
-    res.status(200).send({ message: "Bem Vindo(a) a API!" });
-});
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}!`);
