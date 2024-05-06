cfcf002e2cc1e54309bf10d493d5281aa5c557b4
gustavoaraujofs <gustavoaraujofs2526@gmail.com>
2024-05-05 23:13:31 -0300

    Adicionando configurações de cors.

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,10 +3,12 @@ const tasksRoutes = require("./routes/tasksRoutes");
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(tasksRoutes);
 app.use(userRoutes);
 app.use(authRoutes);
